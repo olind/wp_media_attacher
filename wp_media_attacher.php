d094800a42c7cc5f9bb2d0ab97211a1c04ad292f
Ola Lindberg <ola.lindberg@ntig.se>
2022-02-24 21:46:08 +0100

    .

--- wp_media_attacher.php
+++ wp_media_attacher.php
@@ -1,77 +1,116 @@
 <?php
-/*
-Place this file in wp-content/flickr-attacher.php
-To run it you WP-CLI: wp eval-file flickr-attacher.php
-*/
 
-//find_flickr_post();
-//get_attachments();
-get_all_unattached_media();
+$unattached = get_all_unattached_media();
+_p('Unattached media count: ' . count($unattached));
 
-function d_array($array){
-	WP_CLI::debug(print_r($array, true),  $group = false);
-}
-function d_string($string){
-	WP_CLI::debug(print_r($string, true),  $group = false);
-}
+foreach($unattached as $ua){
 
-function get_attachments(){
-	$media = get_attached_media( 'image', 19147 /*get_the_post()*/ );
-	d_array($media);
+	$filename = get_filename_from_attachment($ua);
+	_p("Searching matching posts for attachment: '" . $filename ."' (ID: " . $ua->ID . ")");
 
-	foreach ($media as $image) {  
-		$ximage =  wp_get_attachment_image_src($image->ID,'medium');
-		WP_CLI::debug($ximage[0]);
+	$matching_post = search_post_content($filename);
+
+	if($matching_post){
+		_p("   Matching post: '" . $matching_post[0]->post_title . "' (ID: " . $matching_post[0]->ID . ")");
+	} else {
+		
 	}
 	
+}
+
+wp_reset_postdata();
+
+/* - - - - - */
+
+/**
+ * Attaches the given media attachment to the given post
+ */
+function attach_media_to_post($media, $post){
+	$args = array(
+		'ID' => $media->ID,);
+
+	_p(wp_insert_attachment($args, false, $post->ID));
+}
+
+/**
+ * Prints a message when the script is run with --debug. 
+ * Accepts both strings and arrays.
+ */
+function _p($array){
+	WP_CLI::debug(print_r($array, true),  $group = false);
+}
+
+/**
+ * Gets filename without extension from full filepath
+ */
+function get_filename_from_attachment($attachment){
+	return pathinfo(get_attached_file($attachment->ID))['filename'];
 }
 
 /**
  * Get a list of all unatached media files in the library
  */
 function get_all_unattached_media(){
-	$args = array(
+	$the_query = array(
 		'post_type' => 'attachment',
-		'numberposts' => -1,
+		'numberposts' => /*-1*/15,
 		'post_status' => null,
 		'post_parent' => 0
 	); 
-	$attachments = get_posts($args);
-
-	d_string('Unattached files count: ' . count($attachments));
-	 
-	 if ($attachments) {
-		foreach ($attachments as $post) {
-			setup_postdata($post);
-			//d_array($post);
-			//d_string(wp_get_attachment_link($post->ID));
-		}
-	 }
+	$attachments = get_posts($the_query);
+	return $attachments;
 }
 
-function find_flickr_post (){
+/** 
+ * Searches all posts for given string. 
+ * Returns array of matching posts or null if no posts are found.
+ */
+function search_post_content($search_string){
+	global $wpdb;
+	$sql = "SELECT * FROM {$wpdb->posts} WHERE post_content LIKE '%" . $search_string . "%' AND post_type = 'post';";
+	$result = $wpdb->get_results($sql);
+
+  	if ($result){
+		_p("   Matching posts: '" . count($result) . "' - Searching for '" . $search_string . "'");
+		$posts = array();
+		foreach($result as $post){
+			$posts[] = get_post($post->ID);
+		}
+		return $posts;
+  	} else {
+		_p("   No matched posts: " . $search_string);
+		return null;
+	}
+}
+
+function search_post_title ($search_string){
 	WP_CLI::log("Finding posts");
 		
 	$the_query = new WP_Query(array( 
-		's'		=>	'/flickr/',
-		'cat'	=>	1050,
+		's'		=>	$search_string,
 		'posts_per_page' => '-1'
 	));
 	
 	if ( $the_query->have_posts() ) {
 	    while ( $the_query->have_posts() ) {
 	        $the_query->the_post();
-			
-
-	        //WP_CLI::line(" P: " . get_the_title());
-
+	        _p(" P: " . get_the_post());
 	    }
 	} else {
-		WP_CLI::warning("No posts found");
+		_p("No posts found");
 	}
-	
-	WP_CLI::line("Total posts found: " . count( $the_query->posts ));
-	wp_reset_postdata(); //Restore original Post Data
+	_p("Total posts found: " . count( $args->posts ));
 }
+
+
+function get_attachments(){
+$media = get_attached_media( 'image', 19147 /*get_the_post()*/ );
+_p($media);
+foreach ($media as $image) {  
+	$ximage =  wp_get_attachment_image_src($image->ID,'medium');
+	_p($ximage[0]);
+}
+}
+
 
 ?>
